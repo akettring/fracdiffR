cd2a3c116b4258623505e7dd1aa23e9f461e7ce2
akettring <akettring@gmail.com>
2021-08-22 22:49:55 -0400

    initial commit 4

--- R/main.R
+++ R/main.R
@@ -1,8 +1,8 @@
 
-# fractional differnecing with fixed width window
+# fractional differnecing of xts timeseries with fixed width window
 # ... as described by Prado, and here (for python) ...
 # https://medium.com/swlh/fractionally-differentiated-features-9c1947ed2b55
-
+# ... by default, series is transformed by ROC "on the fly"
 
 library("tidyverse")
 library("reshape2")
@@ -10,26 +10,26 @@ library("tidyquant")
 
 
 # get a series of weights for fractional difference within a fixed window
-# by default window size is defined by a cutoff for the smallest weight
-# alternatively the window size may be explicitly defined
-get_weights_FD <- function(d=0.25, threshold=1e-4, window){
+# 1. by default window size is defined by a cutoff for the smallest weight
+# 2. alternatively the window size may be explicitly defined
+get_weights_FD <- function(d=0.5, threshold=1e-4, window){
 
-    # intial values for weight series
+    # 0. intialize values for weight series
     w <- c(1.0)
     w_i <- 1.0
     k <- 1
 
-     # if window is explicitly defined ...
-    if (window) {
-        while( k < window ){
+     # 1. define window size by threshold cutoff ...
+    if (missing(window)) {
+        while(  abs(w_i) > threshold ){
             w_i <- -tail(w,1)/k*(d-k+1)
             w <- c(w,w_i)
             k <- k+1
         }
 
-    # otherwise, window size is defined by the threshold
+    # 2. otherwise if window is explicitly defined ...
     } else {
-        while(  abs(w_i) > threshold ){
+        while( k < window ){
             w_i <- -tail(w,1)/k*(d-k+1)
             w <- c(w,w_i)
             k <- k+1
@@ -40,32 +40,46 @@ get_weights_FD <- function(d=0.25, threshold=1e-4, window){
 }
 
 
-# apply FD to a series (x)
-transform_FD <- function(x, d=0.25, window=400){
+# fractionally differentiate an xts timseries
+transform_FD <- function(x, trans_by_roc=TRUE, d=0.5, threshold=1e-4, window){
+
+    # get the weights for transformation
+    w <- get_weights_FD(d, threshold=threshold, window=window)
 
     # widow size can't be larger than the size of data
-    if (window > length(x)){
-        print("too short")
+    if (length(w) > length(x)){
+        print("Series is too short to differnetiate with given parameters!")
         break
     }
 
-    # Apply weights to values
-    w <- get_weights_FD(d, window)
-
+    # transform the timeseries using the weights
     df <- xts(order.by = index(x))
     for (i in seq(length(w),length(x))){
-        # get the indexes
+        # get the timeseries indexes
         i_0_index <- index(x)[i-length(w)+1]    # first time point
         i_1_index <- index(x)[i]                # last time point
-        # get the data
+        # get the timeseries data
         data <- x[between(index(x),i_0_index,i_1_index)] %>% rev()
-        # transform by rolling cusum of %ROC
-        data <- ROC(data, type="discrete") * 100
-        data <- data %>% as.vector() %>% replace_na(0) %>% cumsum()
+
+        # by default, transform by rolling cusum of %ROC
+        if (trans_by_roc==TRUE){
+            data <- ROC(data, type="discrete") * 100
+            data <- data %>% as.vector() %>% replace_na(0) %>% cumsum()
+        }
         # dot product is fractional difference
         dotprod <- w %*% data
         df[i_1_index] <- dotprod
     }
 
     return(df)
+}
+
+
+# Demo the FD function using default settings
+# on FB closing prices from the FANG dataset
+demo_FD <- function(){
+    x <- FANG %>% subset(symbol == "FB")
+    x <- xts(x$close, order.by=x$date)
+    x <- cbind.xts(x, transform_FD(x))
+    return(x)
 }
